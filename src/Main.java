ea01d9771ed3cc3d17634aa22d8c47459271f915
vedrancop <146172096+vedrancop@users.noreply.github.com>
2023-11-22 10:23:29 +0100

    git init

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.xml.namespace.QName;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource( "Scene.fxml"));
+        Parent root = loader.load();
+        Scene scene= new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
